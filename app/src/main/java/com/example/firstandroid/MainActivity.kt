12bc0e95163e34884c83acea5f9a276843e7cd3d
Frank <f.neumann.berlin@googlemail.com>
2023-01-16 13:39:41 +0100

    Design

--- app/src/main/java/com/example/firstandroid/MainActivity.kt
+++ app/src/main/java/com/example/firstandroid/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.firstandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val input = findViewById<EditText>(R.id.et_input)
+        val output = findViewById<TextView>(R.id.tv_output)
+        val btn = findViewById<Button>(R.id.btn_copy)
+
+        btn.setOnClickListener {
+            output.text = input.text
+        }
     }
 }
